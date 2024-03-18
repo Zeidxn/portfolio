44b2e856560b16d7f449d7d16a51352b7c4f28db
NKT04 <88687413+NKT04@users.noreply.github.com>
2024-03-18 20:52:44 +0100

    portfolio home termine

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,174 +1,23 @@
 import React from "react";
-import styled from 'styled-components';
-import {Bio} from "../../data/constants";
-import Typewriter from "typewriter-effect";
-
-const HomeContainer = styled.div`
-  background: ${({ theme }) => theme.card_light};
-  display: flex;
-  justify-content: center;
-  position: relative;
-  padding: 80px 30px;
-  @media (max-width: 960px) {
-    padding: 66px 16px;
-  }
-  @media (max-width: 640px) {
-    padding: 32px 16px;
-  }
-  z-index: 1;
-
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
-`;
-
-const HomeBg = styled.div`
-    position: absolute;
-    display: flex;
-    justify-content: end;
-    top: 50%;
-    right: 0;
-    bottom: 0;
-    left: 50%;
-    width: 100%;
-    height: 100%;
-    max-width: 1360px;
-    overflow: hidden;
-    padding: 0 30px;
-    -webkit-transform: translateX(-50%) translateY(-50%);
-    transform: translateX(-50%) translateY(-50%);
-
-    @media (max-width: 960px) {
-        justify-content: center;
-        padding: 0 0;
-    }
-`;
-
-const HomeInnerContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  max-width: 1100px;
-
-  @media (max-width: 960px) {
-    flex-direction: column;
-  }
-`;
-const HomeLeftContainer = styled.div`
-  width: 100%;
-  order: 1;
-  @media (max-width: 960px) {
-    order: 2;
-    margin-bottom: 30px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  @media (max-width: 640px) {
-    order: 2;
-    margin-bottom: 30px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-const HomeRightContainer = styled.div`
-  width: 100%;
-  display: flex;
-  order: 2;
-  justify-content: end;
-  gap: 12px;
-  @media (max-width: 960px) {
-    order: 1;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 80px;
-  }
-
-  @media (max-width: 640px) {
-    margin-bottom: 30px;
-  }
-`;
-
-const Title = styled.div`
-  font-weight: 700;
-  font-size: 50px;
-  color: ${({ theme }) => theme.text_primary};
-  line-height: 68px;
-  @media (max-width: 960px) {
-    text-align: center;
-  }
-
-  @media (max-width: 640px) {
-    font-size: 40px;
-    line-height: 48px;
-    margin-bottom: 8px;
-  }
-`;
-
-
-const Description = styled.div`
-  font-weight: 600;
-  font-size: 30px;
-  display: flex;
-  gap: 5px;
-  color: ${({ theme }) => theme.text_primary};
-  line-height: 68px;
-    white-space: nowrap;
-    @media (max-width: 960px) {
-    text-align: center;
-  }
-  @media (max-width: 640px) {
-    font-size: 22px;
-    line-height: 48px;
-    margin-bottom: 16px;
-      white-space: normal;
-  }
-`;
-
-const Span = styled.span`
-  color: ${({ theme }) => theme.primary};
-  cursor: pointer;
-
-
-`;
+import { Bio } from "../../data/constants";
+import HomeImg from "../../assets/img/me.jpg";
+import "./home.css";
 
 const Home = () => {
-    return(
-            <div id="about">
-                <HomeContainer>
-                    <HomeBg>
-                    </HomeBg>
-                    <HomeInnerContainer>
-                        <HomeLeftContainer>
-                            <Title>
-                                Bienvenue, Je m'appelle <br/>
-                                {Bio.name}
-                            </Title>
-                            <Description> Je suis un
-                                <Span>
-                                    <Typewriter
-                                        options={{
-                                            strings: Bio.roles,
-                                            autoStart: true,
-                                            loop: true,
-
-
-                                        }}
-
-                                    />
-                                </Span>
-
-                            </Description>
-                        </HomeLeftContainer>
-                        <HomeRightContainer></HomeRightContainer>
-                    </HomeInnerContainer>
-
-                </HomeContainer>
+    return (
+        <div id="about" className="home-container">
+            <div className="home-bg"></div>
+            <div className="home-inner-container">
+                <div className="home-left-container">
+                    <div className="title">{Bio.name}</div>
+                    <div className="subtitle">{Bio.description}</div>
+                </div>
+                <div className="home-right-container">
+                    <img src={HomeImg} alt="Home" className="image" />
+                </div>
             </div>
-        );
-    };
+        </div>
+    );
+};
 
 export default Home;
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,10 +1,7 @@
 export const Bio = {
     name: "Nicolas Toupin",
-    roles: [
-      "Etudiant en BUT Informatique"
-    ],
     description:
-      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+      "Étudiant en informatique à l'Université Côte d'Azur",
     github: "https://github.com/zeidxn",
   
     
